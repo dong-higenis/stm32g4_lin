a1d8956bfa95e9dabb492e633499c6a9a0c2bc5d
dong-higenis <dong@higenis.com>
2024-07-06 17:22:19 +0900

    first commit

---
+++ firmware/stm32g4_lin/App/hw/hw.h
@@ -0,0 +1,30 @@
+#ifndef HW_H_
+#define HW_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_def.h"
+
+
+//==============   HW   ===============
+#include "led.h"
+#include "uart.h"
+#include "cli.h"
+#include "log.h"
+#include "swtimer.h"
+#include "usb.h"
+#include "cdc.h"
+	//============== COMMON ===============
+#include "qbuffer.h"
+#include "util.h"
+
+bool hwInit(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
